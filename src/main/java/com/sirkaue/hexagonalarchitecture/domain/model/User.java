29e0d2455f5e3603c97a9383238f8a326f9043d3
sirkaue <kauekdslopes@gmail.com>
2025-03-12 00:19:55 -0300

    refactor: tornar o modelo User imutável e final

--- src/main/java/com/sirkaue/hexagonalarchitecture/domain/model/User.java
+++ src/main/java/com/sirkaue/hexagonalarchitecture/domain/model/User.java
@@ -2,13 +2,10 @@ package com.sirkaue.hexagonalarchitecture.domain.model;
 
 public class User {
 
-    private Long id;
-    private String name;
-    private String email;
-    private String password;
-
-    public User() {
-    }
+    private final Long id;
+    private final String name;
+    private final String email;
+    private final String password;
 
     public User(Long id, String name, String email, String password) {
         this.id = id;
@@ -21,31 +18,15 @@ public class User {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
