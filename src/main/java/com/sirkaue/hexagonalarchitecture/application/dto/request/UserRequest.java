cc995afd16c19c65fc5e68a9f25ab1adf52ce97e
sirkaue <kauekdslopes@gmail.com>
2025-03-15 18:25:49 -0300

    feat: dtos

---
+++ src/main/java/com/sirkaue/hexagonalarchitecture/application/dto/request/UserRequest.java
@@ -0,0 +1,4 @@
+package com.sirkaue.hexagonalarchitecture.application.dto.request;
+
+public record UserRequest(String name, String email, String password) {
+}
---
+++ src/main/java/com/sirkaue/hexagonalarchitecture/application/dto/response/UserResponse.java
@@ -0,0 +1,4 @@
+package com.sirkaue.hexagonalarchitecture.application.dto.response;
+
+public record UserResponse(Long id, String name, String email) {
+}
