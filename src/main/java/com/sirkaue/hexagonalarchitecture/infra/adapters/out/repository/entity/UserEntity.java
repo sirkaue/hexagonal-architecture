ba1970c27f257550b9f4f15410e9f2a75e1c6e57
sirkaue <kauekdslopes@gmail.com>
2025-03-30 20:41:56 -0300

    feat: adicionar entidade UserEntity para persistência de usuários

---
+++ src/main/java/com/sirkaue/hexagonalarchitecture/infra/adapters/out/repository/entity/UserEntity.java
@@ -0,0 +1,23 @@
+package com.sirkaue.hexagonalarchitecture.infra.adapters.out.repository.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+}
