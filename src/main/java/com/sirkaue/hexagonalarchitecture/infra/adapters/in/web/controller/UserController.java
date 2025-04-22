800b8c9bf653cb14170eecd91c2d1fd46b1b07e5
sirkaue <kauekdslopes@gmail.com>
2025-04-22 16:57:32 -0300

    feat: adiciona endpoint para busca de usuário por ID

--- src/main/java/com/sirkaue/hexagonalarchitecture/infra/adapters/in/web/controller/UserController.java
+++ src/main/java/com/sirkaue/hexagonalarchitecture/infra/adapters/in/web/controller/UserController.java
@@ -1,16 +1,15 @@
 package com.sirkaue.hexagonalarchitecture.infra.adapters.in.web.controller;
 
 import com.sirkaue.hexagonalarchitecture.application.dto.request.UserRequest;
+import com.sirkaue.hexagonalarchitecture.application.dto.response.UserResponse;
 import com.sirkaue.hexagonalarchitecture.application.mapper.UserMapper;
+import com.sirkaue.hexagonalarchitecture.application.ports.in.FindUserByIdUseCase;
 import com.sirkaue.hexagonalarchitecture.application.ports.in.InsertUserUseCase;
 import com.sirkaue.hexagonalarchitecture.domain.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -19,11 +18,19 @@ public class UserController {
 
     private final UserMapper userMapper;
     private final InsertUserUseCase insertUserUseCase;
+    private final FindUserByIdUseCase findUserByIdUseCase;
 
     @PostMapping
     public ResponseEntity<Void> insert(@RequestBody UserRequest userRequest) {
         User user = userMapper.toUser(userRequest);
         insertUserUseCase.execute(user);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> findById(@PathVariable final Long id) {
+        User user = findUserByIdUseCase.execute(id);
+        UserResponse response = userMapper.toUserResponse(user);
+        return ResponseEntity.ok(response);
     }
 }
