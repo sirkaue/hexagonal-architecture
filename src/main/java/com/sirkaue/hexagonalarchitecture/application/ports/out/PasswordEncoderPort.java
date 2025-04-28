3622f7d02a8243807247e3625e30bde07e6124ad
sirkaue <kauekdslopes@gmail.com>
2025-04-28 17:59:32 -0300

    feat: adicionar método matches na porta PasswordEncoderPort

--- src/main/java/com/sirkaue/hexagonalarchitecture/application/ports/out/PasswordEncoderPort.java
+++ src/main/java/com/sirkaue/hexagonalarchitecture/application/ports/out/PasswordEncoderPort.java
@@ -3,4 +3,6 @@ package com.sirkaue.hexagonalarchitecture.application.ports.out;
 public interface PasswordEncoderPort {
 
     String encode(String rawPassword);
+
+    boolean matches(String rawPassword, String encodedPassword);
 }
