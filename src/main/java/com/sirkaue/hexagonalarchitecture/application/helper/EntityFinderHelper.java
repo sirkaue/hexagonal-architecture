00c714c610aef417b93426c645bc4362b39a1805
sirkaue <kauekdslopes@gmail.com>
2025-04-28 17:53:08 -0300

    feat: criar helper para busca de usuário por ID com tratamento de exceção

---
+++ src/main/java/com/sirkaue/hexagonalarchitecture/application/helper/EntityFinderHelper.java
@@ -0,0 +1,19 @@
+package com.sirkaue.hexagonalarchitecture.application.helper;
+
+import com.sirkaue.hexagonalarchitecture.application.ports.out.FindUserByIdPort;
+import com.sirkaue.hexagonalarchitecture.domain.exception.UserNotFoundException;
+import com.sirkaue.hexagonalarchitecture.domain.model.User;
+
+public class EntityFinderHelper {
+
+    private final FindUserByIdPort findUserByIdPort;
+
+    public EntityFinderHelper(FindUserByIdPort findUserByIdPort) {
+        this.findUserByIdPort = findUserByIdPort;
+    }
+
+    public User findUserByIdOrThrow(Long id) {
+        return findUserByIdPort.findById(id).orElseThrow(
+                () -> new UserNotFoundException(String.format("User with id %s not found", id)));
+    }
+}
