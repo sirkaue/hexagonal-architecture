c56c562659d1397e06f052ed5d410cad5e7cd44f
sirkaue <kauekdslopes@gmail.com>
2025-05-02 11:35:56 -0300

    refactor: extrai lógica de busca de usuário para helper reutilizável

--- src/main/java/com/sirkaue/hexagonalarchitecture/application/usecase/FindUserByIdUseCaseImpl.java
+++ src/main/java/com/sirkaue/hexagonalarchitecture/application/usecase/FindUserByIdUseCaseImpl.java
@@ -1,8 +1,7 @@
 package com.sirkaue.hexagonalarchitecture.application.usecase;
 
+import com.sirkaue.hexagonalarchitecture.application.helper.EntityFinderHelper;
 import com.sirkaue.hexagonalarchitecture.application.ports.in.FindUserByIdUseCase;
-import com.sirkaue.hexagonalarchitecture.application.ports.out.FindUserByIdPort;
-import com.sirkaue.hexagonalarchitecture.domain.exception.UserNotFoundException;
 import com.sirkaue.hexagonalarchitecture.domain.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,16 +10,15 @@ public class FindUserByIdUseCaseImpl implements FindUserByIdUseCase {
 
     private static final Logger log = LoggerFactory.getLogger(FindUserByIdUseCaseImpl.class);
 
-    private final FindUserByIdPort findUserByIdPort;
+    private final EntityFinderHelper entityFinderHelper;
 
-    public FindUserByIdUseCaseImpl(FindUserByIdPort findUserByIdPort) {
-        this.findUserByIdPort = findUserByIdPort;
+    public FindUserByIdUseCaseImpl(EntityFinderHelper entityFinderHelper) {
+        this.entityFinderHelper = entityFinderHelper;
     }
 
     @Override
     public User execute(Long id) {
         log.info("Finding user with id: {}", id);
-        return findUserByIdPort.findById(id).orElseThrow(
-                () -> new UserNotFoundException(String.format("User with id %s not found", id)));
+        return entityFinderHelper.findUserByIdOrThrow(id);
     }
 }
