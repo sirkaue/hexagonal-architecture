18554c763b33611faccd39972b46850227e529e9
sirkaue <kauekdslopes@gmail.com>
2025-05-07 20:56:46 -0300

    feat: adicionar criptografia de senha

--- src/main/java/com/sirkaue/hexagonalarchitecture/application/usecase/InsertUserUseCaseImpl.java
+++ src/main/java/com/sirkaue/hexagonalarchitecture/application/usecase/InsertUserUseCaseImpl.java
@@ -2,29 +2,39 @@ package com.sirkaue.hexagonalarchitecture.application.usecase;
 
 import com.sirkaue.hexagonalarchitecture.application.ports.in.InsertUserUseCase;
 import com.sirkaue.hexagonalarchitecture.application.ports.out.InsertUserPort;
+import com.sirkaue.hexagonalarchitecture.application.ports.out.PasswordEncoderPort;
 import com.sirkaue.hexagonalarchitecture.application.ports.out.UserExistsByEmailPort;
 import com.sirkaue.hexagonalarchitecture.domain.exception.EmailAlreadyExistsException;
 import com.sirkaue.hexagonalarchitecture.domain.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class InsertUserUseCaseImpl implements InsertUserUseCase {
 
+    private static final Logger log = LoggerFactory.getLogger(InsertUserUseCaseImpl.class);
+
     private final InsertUserPort insertUserPort;
     private final UserExistsByEmailPort userExistsByEmailPort;
+    private final PasswordEncoderPort passwordEncoderPort;
 
-    public InsertUserUseCaseImpl(InsertUserPort insertUserPort, UserExistsByEmailPort userExistsByEmailPort) {
+    public InsertUserUseCaseImpl(InsertUserPort insertUserPort, UserExistsByEmailPort userExistsByEmailPort,
+                                 PasswordEncoderPort passwordEncoderPort) {
         this.insertUserPort = insertUserPort;
         this.userExistsByEmailPort = userExistsByEmailPort;
+        this.passwordEncoderPort = passwordEncoderPort;
     }
 
     @Override
     public void execute(User user) {
-        validateEmail(user.getEmail());
-        insertUserPort.insert(user);
-    }
+        log.info("Creating user {} with email: {}", user.getName(), user.getEmail().value());
 
-    private void validateEmail(String email) {
-        if (userExistsByEmailPort.existsByEmail(email)) {
+        if (userExistsByEmailPort.existsByEmail(user.getEmail().value())) {
             throw new EmailAlreadyExistsException("Email already exists");
         }
+
+        log.info("Encoding password");
+        String hashedPassword = passwordEncoderPort.encode(user.getPassword());
+        user = user.changePasswordTo(hashedPassword);
+        insertUserPort.insert(user);
     }
 }
