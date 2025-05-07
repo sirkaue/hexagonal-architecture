4ea529b4f1331d18a6f383c9c330b6458a7f63ed
sirkaue <kauekdslopes@gmail.com>
2025-05-07 19:59:24 -0300

    feat: adiciona endpoint para atualizar email

--- src/main/java/com/sirkaue/hexagonalarchitecture/infra/adapters/in/web/controller/UserController.java
+++ src/main/java/com/sirkaue/hexagonalarchitecture/infra/adapters/in/web/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.sirkaue.hexagonalarchitecture.infra.adapters.in.web.controller;
 
+import com.sirkaue.hexagonalarchitecture.application.dto.request.UpdateEmailRequest;
 import com.sirkaue.hexagonalarchitecture.application.dto.request.UserRequest;
 import com.sirkaue.hexagonalarchitecture.application.dto.response.UserResponse;
 import com.sirkaue.hexagonalarchitecture.application.mapper.UserMapper;
 import com.sirkaue.hexagonalarchitecture.application.ports.in.FindUserByIdUseCase;
 import com.sirkaue.hexagonalarchitecture.application.ports.in.InsertUserUseCase;
+import com.sirkaue.hexagonalarchitecture.application.ports.in.UpdateUserEmailUseCase;
 import com.sirkaue.hexagonalarchitecture.domain.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,6 +21,7 @@ public class UserController {
     private final UserMapper userMapper;
     private final InsertUserUseCase insertUserUseCase;
     private final FindUserByIdUseCase findUserByIdUseCase;
+    private final UpdateUserEmailUseCase updateUserEmailUseCase;
 
     @PostMapping
     public ResponseEntity<Void> insert(@RequestBody UserRequest userRequest) {
@@ -32,5 +35,11 @@ public class UserController {
         User user = findUserByIdUseCase.execute(id);
         UserResponse response = userMapper.toUserResponse(user);
         return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/{id}/email")
+    public ResponseEntity<Void> updateEmail(@PathVariable final Long id, @RequestBody UpdateEmailRequest request) {
+        updateUserEmailUseCase.execute(id, request.email());
+        return ResponseEntity.noContent().build();
     }
 }
