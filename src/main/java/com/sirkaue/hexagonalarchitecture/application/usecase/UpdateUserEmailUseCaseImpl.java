e90c94c0d8d54a266f1836a8ab10b5b9f9497c71
sirkaue <kauekdslopes@gmail.com>
2025-05-07 20:58:40 -0300

    feat: criar caso de uso para atualização de e-mail de usuário

---
+++ src/main/java/com/sirkaue/hexagonalarchitecture/application/usecase/UpdateUserEmailUseCaseImpl.java
@@ -0,0 +1,51 @@
+package com.sirkaue.hexagonalarchitecture.application.usecase;
+
+import com.sirkaue.hexagonalarchitecture.application.helper.EntityFinderHelper;
+import com.sirkaue.hexagonalarchitecture.application.ports.in.UpdateUserEmailUseCase;
+import com.sirkaue.hexagonalarchitecture.application.ports.out.UpdateUserEmailPort;
+import com.sirkaue.hexagonalarchitecture.application.ports.out.UserExistsByEmailPort;
+import com.sirkaue.hexagonalarchitecture.domain.exception.EmailAlreadyExistsException;
+import com.sirkaue.hexagonalarchitecture.domain.model.User;
+import com.sirkaue.hexagonalarchitecture.domain.valueobjects.Email;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UpdateUserEmailUseCaseImpl implements UpdateUserEmailUseCase {
+
+    private static final Logger log = LoggerFactory.getLogger(UpdateUserEmailUseCaseImpl.class);
+
+    private final EntityFinderHelper entityFinderHelper;
+    private final UserExistsByEmailPort userExistsByEmailPort;
+    private final UpdateUserEmailPort updateUserEmailPort;
+
+    public UpdateUserEmailUseCaseImpl(EntityFinderHelper entityFinderHelper, UserExistsByEmailPort userExistsByEmailPort,
+                                      UpdateUserEmailPort updateUserEmailPort) {
+        this.entityFinderHelper = entityFinderHelper;
+        this.userExistsByEmailPort = userExistsByEmailPort;
+        this.updateUserEmailPort = updateUserEmailPort;
+    }
+
+
+    @Override
+    public User execute(Long id, String newEmail) {
+        log.info("Finding user with id: {}", id);
+        User user = entityFinderHelper.findUserByIdOrThrow(id);
+
+        log.info("Validating email");
+        validateEmail(new Email(newEmail), user);
+
+        log.info("Updating email for user with id: {}", id);
+        User updatedUser = user.changeEmailTo(newEmail);
+        return updateUserEmailPort.update(updatedUser);
+    }
+
+    private void validateEmail(Email newEmail, User user) {
+        if (user.getEmail().equals(newEmail)) {
+            throw new EmailAlreadyExistsException("The new email is the same as the current one");
+        }
+
+        if (userExistsByEmailPort.existsByEmail(newEmail.value())) {
+            throw new EmailAlreadyExistsException("Email already exists");
+        }
+    }
+}
