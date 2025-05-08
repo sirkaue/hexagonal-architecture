895382fb0ff1fc53f84ee285825a2cc4d752ad7e
sirkaue <kauekdslopes@gmail.com>
2025-05-07 21:04:00 -0300

    feat: atualizar contrato do caso de uso de atualização de senha para utilizar `Password` como VO

--- src/main/java/com/sirkaue/hexagonalarchitecture/application/ports/in/UpdateUserPasswordUseCase.java
+++ src/main/java/com/sirkaue/hexagonalarchitecture/application/ports/in/UpdateUserPasswordUseCase.java
@@ -1,8 +1,9 @@
 package com.sirkaue.hexagonalarchitecture.application.ports.in;
 
 import com.sirkaue.hexagonalarchitecture.domain.model.User;
+import com.sirkaue.hexagonalarchitecture.domain.valueobjects.Password;
 
 public interface UpdateUserPasswordUseCase {
 
-    User execute(Long id, String currentPassword, String newPassword, String confirmPassword);
+    User execute(Long id, Password currentPassword, Password newPassword, Password confirmPassword);
 }
