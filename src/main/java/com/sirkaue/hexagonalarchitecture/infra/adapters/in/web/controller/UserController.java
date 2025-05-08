1720781d1f63cb89d73dc633a5c1436ec221d1fe
sirkaue <kauekdslopes@gmail.com>
2025-05-08 20:37:30 -0300

    feat: adicionar endpoint para atualização de senha no UserController

--- src/main/java/com/sirkaue/hexagonalarchitecture/infra/adapters/in/web/controller/UserController.java
+++ src/main/java/com/sirkaue/hexagonalarchitecture/infra/adapters/in/web/controller/UserController.java
@@ -1,13 +1,16 @@
 package com.sirkaue.hexagonalarchitecture.infra.adapters.in.web.controller;
 
-import com.sirkaue.hexagonalarchitecture.application.dto.request.UpdateEmailRequest;
-import com.sirkaue.hexagonalarchitecture.application.dto.request.UserRequest;
-import com.sirkaue.hexagonalarchitecture.application.dto.response.UserResponse;
-import com.sirkaue.hexagonalarchitecture.application.mapper.UserMapper;
 import com.sirkaue.hexagonalarchitecture.application.ports.in.FindUserByIdUseCase;
 import com.sirkaue.hexagonalarchitecture.application.ports.in.InsertUserUseCase;
 import com.sirkaue.hexagonalarchitecture.application.ports.in.UpdateUserEmailUseCase;
+import com.sirkaue.hexagonalarchitecture.application.ports.in.UpdateUserPasswordUseCase;
 import com.sirkaue.hexagonalarchitecture.domain.model.User;
+import com.sirkaue.hexagonalarchitecture.domain.valueobjects.Password;
+import com.sirkaue.hexagonalarchitecture.infra.adapters.in.dto.request.UpdateEmailRequest;
+import com.sirkaue.hexagonalarchitecture.infra.adapters.in.dto.request.UpdatePasswordRequest;
+import com.sirkaue.hexagonalarchitecture.infra.adapters.in.dto.request.UserRequest;
+import com.sirkaue.hexagonalarchitecture.infra.adapters.in.dto.response.UserResponse;
+import com.sirkaue.hexagonalarchitecture.infra.adapters.in.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +25,7 @@ public class UserController {
     private final InsertUserUseCase insertUserUseCase;
     private final FindUserByIdUseCase findUserByIdUseCase;
     private final UpdateUserEmailUseCase updateUserEmailUseCase;
+    private final UpdateUserPasswordUseCase updateUserPasswordUseCase;
 
     @PostMapping
     public ResponseEntity<Void> insert(@RequestBody UserRequest userRequest) {
@@ -40,6 +44,13 @@ public class UserController {
     @PatchMapping("/{id}/email")
     public ResponseEntity<Void> updateEmail(@PathVariable final Long id, @RequestBody UpdateEmailRequest request) {
         updateUserEmailUseCase.execute(id, request.email());
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}/password")
+    public ResponseEntity<Void> updatePassword(@PathVariable final Long id, @RequestBody UpdatePasswordRequest request) {
+        updateUserPasswordUseCase.execute(id, new Password(request.currentPassword()), new Password(request.newPassword()),
+                new Password(request.confirmPassword()));
         return ResponseEntity.noContent().build();
     }
 }
