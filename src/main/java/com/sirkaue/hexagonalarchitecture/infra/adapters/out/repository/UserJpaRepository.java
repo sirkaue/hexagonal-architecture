087a28f656a0de0b892558a6a555f5f24ce86e89
sirkaue <kauekdslopes@gmail.com>
2025-05-08 21:00:41 -0300

    refactor: renomear UserRepository para UserJpaRepository

--- src/main/java/com/sirkaue/hexagonalarchitecture/infra/adapters/out/repository/UserJpaRepository.java
+++ src/main/java/com/sirkaue/hexagonalarchitecture/infra/adapters/out/repository/UserJpaRepository.java
@@ -3,7 +3,7 @@ package com.sirkaue.hexagonalarchitecture.infra.adapters.out.repository;
 import com.sirkaue.hexagonalarchitecture.infra.adapters.out.persistence.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
 
     boolean existsByEmail(String email);
 }
