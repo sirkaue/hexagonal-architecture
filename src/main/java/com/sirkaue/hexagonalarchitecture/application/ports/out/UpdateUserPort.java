cf68d6a0a36b7b59792a989879d583164aed29a7
sirkaue <kauekdslopes@gmail.com>
2025-05-10 16:09:28 -0300

    refactor: renomear UpdateUserPasswordPort para UpdateUserPort

--- src/main/java/com/sirkaue/hexagonalarchitecture/application/ports/out/UpdateUserPort.java
+++ src/main/java/com/sirkaue/hexagonalarchitecture/application/ports/out/UpdateUserPort.java
@@ -2,7 +2,7 @@ package com.sirkaue.hexagonalarchitecture.application.ports.out;
 
 import com.sirkaue.hexagonalarchitecture.domain.model.User;
 
-public interface UpdateUserPasswordPort {
+public interface UpdateUserPort {
 
     User update(User user);
 }
