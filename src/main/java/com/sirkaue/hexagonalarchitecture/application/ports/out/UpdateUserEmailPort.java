21bf58b788dabda8d17fe52c0ac63ad78338b870
sirkaue <kauekdslopes@gmail.com>
2025-05-10 16:06:10 -0300

    remove: excluir porta UpdateUserEmailPort

--- src/main/java/com/sirkaue/hexagonalarchitecture/application/ports/out/UpdateUserEmailPort.java
+++
@@ -1,8 +0,0 @@
-package com.sirkaue.hexagonalarchitecture.application.ports.out;
-
-import com.sirkaue.hexagonalarchitecture.domain.model.User;
-
-public interface UpdateUserEmailPort {
-
-    User update(User user);
-}
