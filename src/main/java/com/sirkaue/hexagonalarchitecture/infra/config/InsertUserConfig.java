d81f578ce0ab92b8219142920824ba971f8dd78d
sirkaue <kauekdslopes@gmail.com>
2025-05-10 15:26:08 -0300

    feat: adicionar PasswordEncoderPort na configuração de InsertUserUseCase

--- src/main/java/com/sirkaue/hexagonalarchitecture/infra/config/InsertUserConfig.java
+++ src/main/java/com/sirkaue/hexagonalarchitecture/infra/config/InsertUserConfig.java
@@ -2,6 +2,7 @@ package com.sirkaue.hexagonalarchitecture.infra.config;
 
 import com.sirkaue.hexagonalarchitecture.application.ports.in.InsertUserUseCase;
 import com.sirkaue.hexagonalarchitecture.application.ports.out.InsertUserPort;
+import com.sirkaue.hexagonalarchitecture.application.ports.out.PasswordEncoderPort;
 import com.sirkaue.hexagonalarchitecture.application.ports.out.UserExistsByEmailPort;
 import com.sirkaue.hexagonalarchitecture.application.usecase.InsertUserUseCaseImpl;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +12,8 @@ import org.springframework.context.annotation.Configuration;
 public class InsertUserConfig {
 
     @Bean
-    public InsertUserUseCase insertUserUseCase(InsertUserPort insertUserPort, UserExistsByEmailPort userExistsByEmailPort) {
-        return new InsertUserUseCaseImpl(insertUserPort, userExistsByEmailPort);
+    public InsertUserUseCase insertUserUseCase(InsertUserPort insertUserPort, UserExistsByEmailPort userExistsByEmailPort,
+                                               PasswordEncoderPort passwordEncoderPort) {
+        return new InsertUserUseCaseImpl(insertUserPort, userExistsByEmailPort, passwordEncoderPort);
     }
 }
