17779fbd9fe18e8f4dc825ab2f935aa4d7869ab0
sirkaue <kauekdslopes@gmail.com>
2025-05-10 16:17:14 -0300

    refactor: renomear UpdateUserEmailPort para UpdateUserPort na configuração de UpdateUserEmailUseCase

--- src/main/java/com/sirkaue/hexagonalarchitecture/infra/config/UpdateUserEmailConfig.java
+++ src/main/java/com/sirkaue/hexagonalarchitecture/infra/config/UpdateUserEmailConfig.java
@@ -2,9 +2,10 @@ package com.sirkaue.hexagonalarchitecture.infra.config;
 
 import com.sirkaue.hexagonalarchitecture.application.helper.EntityFinderHelper;
 import com.sirkaue.hexagonalarchitecture.application.ports.in.UpdateUserEmailUseCase;
-import com.sirkaue.hexagonalarchitecture.application.ports.out.UpdateUserEmailPort;
+import com.sirkaue.hexagonalarchitecture.application.ports.out.UpdateUserPort;
 import com.sirkaue.hexagonalarchitecture.application.ports.out.UserExistsByEmailPort;
 import com.sirkaue.hexagonalarchitecture.application.usecase.UpdateUserEmailUseCaseImpl;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +14,7 @@ public class UpdateUserEmailConfig {
 
     @Bean
     public UpdateUserEmailUseCase updateUserEmailUseCase(EntityFinderHelper entityFinderHelper, UserExistsByEmailPort userExistsByEmailPort,
-                                                         UpdateUserEmailPort updateUserEmailPort) {
-        return new UpdateUserEmailUseCaseImpl(entityFinderHelper, userExistsByEmailPort, updateUserEmailPort);
+                                                         @Qualifier("updateUserEmailAdapter") UpdateUserPort updateUserPort) {
+        return new UpdateUserEmailUseCaseImpl(entityFinderHelper, userExistsByEmailPort, updateUserPort);
     }
 }
