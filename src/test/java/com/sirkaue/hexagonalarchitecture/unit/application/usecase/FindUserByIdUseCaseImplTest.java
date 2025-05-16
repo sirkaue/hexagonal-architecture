e2ced18ec3bcbd21f41a65c131215d96c1ee221d
sirkaue <kauekdslopes@gmail.com>
2025-05-15 21:53:54 -0300

    refactor: mover FindUserByIdUseCaseImplTest para o pacote unit

--- src/test/java/com/sirkaue/hexagonalarchitecture/unit/application/usecase/FindUserByIdUseCaseImplTest.java
+++ src/test/java/com/sirkaue/hexagonalarchitecture/unit/application/usecase/FindUserByIdUseCaseImplTest.java
@@ -1,6 +1,7 @@
-package com.sirkaue.hexagonalarchitecture.application.usecase;
+package com.sirkaue.hexagonalarchitecture.unit.application.usecase;
 
 import com.sirkaue.hexagonalarchitecture.application.helper.EntityFinderHelper;
+import com.sirkaue.hexagonalarchitecture.application.usecase.FindUserByIdUseCaseImpl;
 import com.sirkaue.hexagonalarchitecture.domain.exception.UserNotFoundException;
 import com.sirkaue.hexagonalarchitecture.domain.model.User;
 import com.sirkaue.hexagonalarchitecture.domain.valueobjects.Email;
