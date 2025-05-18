5660fd9a4400328da371294f1cca4e993057635f
sirkaue <kauekdslopes@gmail.com>
2025-05-18 18:57:41 -0300

    test: garantir atualização da senha do usuário com sucesso

---
+++ src/test/java/com/sirkaue/hexagonalarchitecture/unit/application/usecase/UpdateUserPasswordUseCaseImplTest.java
@@ -0,0 +1,73 @@
+package com.sirkaue.hexagonalarchitecture.unit.application.usecase;
+
+import com.sirkaue.hexagonalarchitecture.application.helper.EntityFinderHelper;
+import com.sirkaue.hexagonalarchitecture.application.ports.out.PasswordEncoderPort;
+import com.sirkaue.hexagonalarchitecture.application.ports.out.UpdateUserPort;
+import com.sirkaue.hexagonalarchitecture.application.usecase.UpdateUserPasswordUseCaseImpl;
+import com.sirkaue.hexagonalarchitecture.domain.model.User;
+import com.sirkaue.hexagonalarchitecture.domain.valueobjects.Email;
+import com.sirkaue.hexagonalarchitecture.domain.valueobjects.Password;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UpdateUserPasswordUseCaseImplTest {
+
+    @Mock
+    private EntityFinderHelper entityFinderHelper;
+
+    @Mock
+    private PasswordEncoderPort passwordEncoderPort;
+
+    @Mock
+    private UpdateUserPort updateUserPort;
+
+    @InjectMocks
+    private UpdateUserPasswordUseCaseImpl updateUserPasswordUseCase;
+
+    @Test
+    void shouldUpdateUserPassword() {
+        // Arrange
+        User user = new User(1L, "John Doe", new Email("teste@teste.com"), new Password("123456"));
+        String newPassword = "admin123";
+        User userWithNewPassword = user.changePasswordTo(newPassword);
+
+        Password currentPassword = new Password("123456");
+        Password newPasswordObj = new Password(newPassword);
+        Password confirmPassword = new Password(newPassword);
+
+        when(entityFinderHelper.findUserByIdOrThrow(anyLong())).thenReturn(user);
+        when(passwordEncoderPort.matches(currentPassword, user.getPassword().value())).thenReturn(true);
+        when(passwordEncoderPort.matches(newPasswordObj, user.getPassword().value())).thenReturn(false);
+        when(passwordEncoderPort.encode(newPasswordObj)).thenReturn(newPassword);
+
+        ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
+        when(updateUserPort.update(userCaptor.capture())).thenReturn(userWithNewPassword);
+
+        // Act
+        User updatedUser = updateUserPasswordUseCase.execute(user.getId(), currentPassword, newPasswordObj, confirmPassword);
+
+        // Assert
+        User savedUser = userCaptor.getValue();
+        assertEquals(user.getId(), savedUser.getId());
+        assertEquals(newPassword, savedUser.getPassword().value());
+
+        assertEquals(user.getId(), updatedUser.getId());
+        assertEquals(newPassword, updatedUser.getPassword().value());
+
+        verify(entityFinderHelper).findUserByIdOrThrow(user.getId());
+        verify(passwordEncoderPort).matches(currentPassword, user.getPassword().value());
+        verify(passwordEncoderPort).matches(newPasswordObj, user.getPassword().value());
+        verify(passwordEncoderPort).encode(newPasswordObj);
+        verify(updateUserPort).update(savedUser);
+    }
+}
