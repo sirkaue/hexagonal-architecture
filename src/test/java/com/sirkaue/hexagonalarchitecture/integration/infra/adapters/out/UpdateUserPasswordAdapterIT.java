e4ebd44f013a78b38f237dd1905b31516a3d70a9
sirkaue <kauekdslopes@gmail.com>
2025-05-25 21:28:50 -0300

    test: adiciona teste de integração para atualização de senha do usuário

---
+++ src/test/java/com/sirkaue/hexagonalarchitecture/integration/infra/adapters/out/UpdateUserPasswordAdapterIT.java
@@ -0,0 +1,46 @@
+package com.sirkaue.hexagonalarchitecture.integration.infra.adapters.out;
+
+import com.sirkaue.hexagonalarchitecture.domain.model.User;
+import com.sirkaue.hexagonalarchitecture.domain.valueobjects.Email;
+import com.sirkaue.hexagonalarchitecture.domain.valueobjects.Password;
+import com.sirkaue.hexagonalarchitecture.infra.adapters.out.UpdateUserPasswordAdapter;
+import com.sirkaue.hexagonalarchitecture.infra.adapters.out.persistence.entity.UserEntity;
+import com.sirkaue.hexagonalarchitecture.infra.adapters.out.persistence.mapper.impl.UserEntityMapperImpl;
+import com.sirkaue.hexagonalarchitecture.integration.infra.adapters.support.helper.UserTestDataHelper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ActiveProfiles("test")
+@DataJpaTest
+@Import({UpdateUserPasswordAdapter.class, UserEntityMapperImpl.class, UserTestDataHelper.class})
+public class UpdateUserPasswordAdapterIT {
+
+    @Autowired
+    private UpdateUserPasswordAdapter adapter;
+
+    @Autowired
+    private UserTestDataHelper helper;
+
+    @Test
+    void shouldUpdateUserPassword() {
+        // Arrange
+        UserEntity userEntity = helper.persistDefaultUser();
+
+        User user = new User(userEntity.getId(), userEntity.getName(),
+                new Email(userEntity.getEmail()), new Password(userEntity.getPassword()));
+
+        String newPassword = "password321";
+        User updatedUser = user.changePasswordTo(newPassword);
+
+        // Act
+        adapter.update(updatedUser);
+
+        // Assert
+        assertEquals(newPassword, updatedUser.getPassword().value());
+    }
+}
