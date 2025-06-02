7f30dc86dd39b39f7b9db79c041e594c13644ff0
sirkaue <kauekdslopes@gmail.com>
2025-06-02 18:42:03 -0300

    feat: adicionar validações no DTO UpdateEmailRequest

--- src/main/java/com/sirkaue/hexagonalarchitecture/infra/adapters/in/dto/request/UpdateEmailRequest.java
+++ src/main/java/com/sirkaue/hexagonalarchitecture/infra/adapters/in/dto/request/UpdateEmailRequest.java
@@ -1,4 +1,11 @@
 package com.sirkaue.hexagonalarchitecture.infra.adapters.in.dto.request;
 
-public record UpdateEmailRequest(String email) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UpdateEmailRequest(
+
+        @NotBlank(message = "Email must not be blank")
+        @Email(message = "Email must be valid")
+        String email) {
 }
