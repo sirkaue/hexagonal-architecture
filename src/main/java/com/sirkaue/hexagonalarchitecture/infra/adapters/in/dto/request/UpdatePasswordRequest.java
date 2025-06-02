0b2ae1bf2f35d17bc02d6cc19b239fb908b698df
sirkaue <kauekdslopes@gmail.com>
2025-06-02 18:42:18 -0300

    feat: adicionar validações no DTO UpdatePasswordRequest

--- src/main/java/com/sirkaue/hexagonalarchitecture/infra/adapters/in/dto/request/UpdatePasswordRequest.java
+++ src/main/java/com/sirkaue/hexagonalarchitecture/infra/adapters/in/dto/request/UpdatePasswordRequest.java
@@ -1,4 +1,19 @@
 package com.sirkaue.hexagonalarchitecture.infra.adapters.in.dto.request;
 
-public record UpdatePasswordRequest(String currentPassword, String newPassword, String confirmPassword) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UpdatePasswordRequest(
+
+        @NotBlank(message = "Password must not be blank")
+        @Size(min = 6, message = "Password must be at least 6 characters")
+        String currentPassword,
+
+        @NotBlank(message = "Password must not be blank")
+        @Size(min = 6, message = "Password must be at least 6 characters")
+        String newPassword,
+
+        @NotBlank(message = "Password must not be blank")
+        @Size(min = 6, message = "Password must be at least 6 characters")
+        String confirmPassword) {
 }
