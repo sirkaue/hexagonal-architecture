713ffbc276ef6992ad0aa62c1f8518dd3b9c8489
sirkaue <kauekdslopes@gmail.com>
2025-06-02 18:41:40 -0300

    feat: adicionar validações no DTO UserRequest

--- src/main/java/com/sirkaue/hexagonalarchitecture/infra/adapters/in/dto/request/UserRequest.java
+++ src/main/java/com/sirkaue/hexagonalarchitecture/infra/adapters/in/dto/request/UserRequest.java
@@ -1,4 +1,19 @@
 package com.sirkaue.hexagonalarchitecture.infra.adapters.in.dto.request;
 
-public record UserRequest(String name, String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserRequest(
+
+        @NotBlank(message = "Name must not be blank")
+        String name,
+
+        @NotBlank(message = "Email must not be blank")
+        @Email(message = "Email must be valid")
+        String email,
+
+        @NotBlank(message = "Password must not be blank")
+        @Size(min = 6, message = "Password must be at least 6 characters")
+        String password) {
 }
