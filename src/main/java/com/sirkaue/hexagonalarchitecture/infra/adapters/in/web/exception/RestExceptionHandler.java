32424d3efe89ed1c6004834371e1baa31c8fcfbf
sirkaue <kauekdslopes@gmail.com>
2025-06-03 11:48:09 -0300

    feat: adicionar tratamento para erros de validação com mensagem detalhada

--- src/main/java/com/sirkaue/hexagonalarchitecture/infra/adapters/in/web/exception/RestExceptionHandler.java
+++ src/main/java/com/sirkaue/hexagonalarchitecture/infra/adapters/in/web/exception/RestExceptionHandler.java
@@ -1,13 +1,17 @@
 package com.sirkaue.hexagonalarchitecture.infra.adapters.in.web.exception;
 
-import com.sirkaue.hexagonalarchitecture.infra.adapters.in.dto.response.ErrorResponse;
 import com.sirkaue.hexagonalarchitecture.domain.exception.*;
+import com.sirkaue.hexagonalarchitecture.infra.adapters.in.dto.response.ErrorResponse;
+import com.sirkaue.hexagonalarchitecture.infra.adapters.in.dto.response.FieldErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
@@ -44,11 +48,29 @@ public class RestExceptionHandler {
         return createErrorResponse(request, HttpStatus.BAD_REQUEST, ex);
     }
 
-    private ResponseEntity<ErrorResponse> createErrorResponse(HttpServletRequest request, HttpStatus status, Exception exception) {
-        ErrorResponse error = new ErrorResponse(request, status, exception);
-        return ResponseEntity
-                .status(status)
-                .contentType(APPLICATION_JSON)
-                .body(error);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex, HttpServletRequest request) {
+
+        List<FieldErrorResponse> errors = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> new FieldErrorResponse(error.getField(), error.getDefaultMessage()))
+                .toList();
+
+        if (errors.size() == 1) {
+            FieldErrorResponse singleError = errors.get(0);
+            ErrorResponse errorResponse = new ErrorResponse(request, HttpStatus.UNPROCESSABLE_ENTITY, singleError.message());
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(errorResponse);
+        }
+
+        ErrorResponse errorResponse = new ErrorResponse(request, HttpStatus.UNPROCESSABLE_ENTITY, errors);
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(errorResponse);
+    }
+
+    private ResponseEntity<ErrorResponse> createErrorResponse(
+            HttpServletRequest request,
+            HttpStatus status,
+            Exception exception
+    ) {
+        ErrorResponse error = new ErrorResponse(request, status, exception.getMessage());
+        return ResponseEntity.status(status).contentType(APPLICATION_JSON).body(error);
     }
 }
