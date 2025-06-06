c8c22bdcbd79f60e1835ccf61396481a2b7cf095
sirkaue <kauekdslopes@gmail.com>
2025-06-05 21:08:06 -0300

    refactor: simplifica ErrorResponse e extrai erros de validação para ValidationErrorResponse

--- src/main/java/com/sirkaue/hexagonalarchitecture/infra/adapters/in/dto/response/ErrorResponse.java
+++ src/main/java/com/sirkaue/hexagonalarchitecture/infra/adapters/in/dto/response/ErrorResponse.java
@@ -1,46 +1,25 @@
 package com.sirkaue.hexagonalarchitecture.infra.adapters.in.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ErrorResponse(
-
         LocalDateTime timestamp,
         int status,
         String error,
         String message,
         String path,
-        String method,
-        List<FieldErrorResponse> errors) {
+        String method) {
 
-    // construtor para mensagem simples
-    public ErrorResponse(HttpServletRequest request, HttpStatus status, String message) {
-        this(
-                LocalDateTime.now(),
-                status.value(),
-                status.getReasonPhrase(),  // preenche o campo 'error'
-                message,
-                request.getRequestURI(),   // pega o path da requisição
-                request.getMethod(),       // pega o método HTTP
-                null                       // sem lista de erros
-        );
-    }
-
-    // construtor para lista de erros
-    public ErrorResponse(HttpServletRequest request, HttpStatus status, List<FieldErrorResponse> errors) {
+    public ErrorResponse(HttpServletRequest request, HttpStatus status, Exception message) {
         this(
                 LocalDateTime.now(),
                 status.value(),
                 status.getReasonPhrase(),
-                null,                      // sem mensagem simples
+                message.getMessage(),
                 request.getRequestURI(),
-                request.getMethod(),
-                errors                     // lista de erros
-        );
+                request.getMethod());
     }
 }
