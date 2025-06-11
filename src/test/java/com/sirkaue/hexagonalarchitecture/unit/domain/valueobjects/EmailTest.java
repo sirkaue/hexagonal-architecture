5c954b1e95e6cf7aa127a04f638f222a6bfcb54e
sirkaue <kauekdslopes@gmail.com>
2025-06-11 20:37:39 -0300

    test: adicionar testes unitários para validar criação e exceções de Email

---
+++ src/test/java/com/sirkaue/hexagonalarchitecture/unit/domain/valueobjects/EmailTest.java
@@ -0,0 +1,31 @@
+package com.sirkaue.hexagonalarchitecture.unit.domain.valueobjects;
+
+import com.sirkaue.hexagonalarchitecture.domain.exception.InvalidEmailException;
+import com.sirkaue.hexagonalarchitecture.domain.valueobjects.Email;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailTest {
+
+    @Test
+    void shouldCreateEmailWhenValid() {
+        String validEmail = "user@example.com";
+        Email email = new Email(validEmail);
+
+        assertNotNull(email);
+        assertEquals(validEmail, email.value());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenEmailIsNull() {
+        assertThrows(InvalidEmailException.class, () -> new Email(null));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenEmailIsInvalid() {
+        assertThrows(InvalidEmailException.class, () -> new Email("invalid-email"));
+        assertThrows(InvalidEmailException.class, () -> new Email("no-at-sign.com"));
+        assertThrows(InvalidEmailException.class, () -> new Email("no-domain@"));
+    }
+}
