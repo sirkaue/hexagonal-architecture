084e70123ce7eebda2b0c540474d011d4fb9b27f
sirkaue <kauekdslopes@gmail.com>
2025-06-10 22:08:07 -0300

    feat: adicionar validação mínima de 6 caracteres no VO Password

--- src/main/java/com/sirkaue/hexagonalarchitecture/domain/valueobjects/Password.java
+++ src/main/java/com/sirkaue/hexagonalarchitecture/domain/valueobjects/Password.java
@@ -1,4 +1,12 @@
 package com.sirkaue.hexagonalarchitecture.domain.valueobjects;
 
+import com.sirkaue.hexagonalarchitecture.domain.exception.InvalidPasswordException;
+
 public record Password(String value) {
+
+    public Password {
+        if (value == null || value.length() < 6) {
+            throw new InvalidPasswordException("Password must be at least 6 characters");
+        }
+    }
 }
