2c007bbfb5ea02b55b3a07476bb9db104ee800c7
sirkaue <kauekdslopes@gmail.com>
2025-06-11 20:38:23 -0300

    test: adicionar testes unitários para validação do Password

---
+++ src/test/java/com/sirkaue/hexagonalarchitecture/unit/domain/valueobjects/PasswordTest.java
@@ -0,0 +1,29 @@
+package com.sirkaue.hexagonalarchitecture.unit.domain.valueobjects;
+
+import com.sirkaue.hexagonalarchitecture.domain.exception.InvalidPasswordException;
+import com.sirkaue.hexagonalarchitecture.domain.valueobjects.Password;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordTest {
+
+    @Test
+    void shouldCreatePasswordWhenValid() {
+        String validPassword = "validPass123";
+        Password password = new Password(validPassword);
+
+        assertNotNull(validPassword);
+        assertEquals(validPassword, password.value());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenPasswordIsTooShort() {
+        assertThrows(InvalidPasswordException.class, () -> new Password("123"));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenPasswordIsNull() {
+        assertThrows(InvalidPasswordException.class, () -> new Password(null));
+    }
+}
