fd95c56d62a3cc1f6dec79e5305ab491d1764028
sirkaue <kauekdslopes@gmail.com>
2025-06-10 22:08:58 -0300

    feat: adicionar validações de nome não nulo, não vazio e tamanho máximo no VO Name

---
+++ src/main/java/com/sirkaue/hexagonalarchitecture/domain/valueobjects/Name.java
@@ -0,0 +1,18 @@
+package com.sirkaue.hexagonalarchitecture.domain.valueobjects;
+
+import com.sirkaue.hexagonalarchitecture.domain.exception.InvalidNameException;
+
+public record Name(String value) {
+
+    public Name {
+        if (value == null || value.isBlank()) {
+            throw new InvalidNameException("Name cannot be null or blank");
+        }
+
+        value = value.trim();
+
+        if (value.length() > 100) {
+            throw new InvalidNameException("Name must not exceed 100 characters");
+        }
+    }
+}
