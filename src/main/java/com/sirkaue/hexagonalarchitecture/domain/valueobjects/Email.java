021ed506b469a300a8244790a8011e38f617f68c
sirkaue <kauekdslopes@gmail.com>
2025-06-10 22:07:58 -0300

    feat: adicionar validação e normalização ao VO Email

--- src/main/java/com/sirkaue/hexagonalarchitecture/domain/valueobjects/Email.java
+++ src/main/java/com/sirkaue/hexagonalarchitecture/domain/valueobjects/Email.java
@@ -1,4 +1,13 @@
 package com.sirkaue.hexagonalarchitecture.domain.valueobjects;
 
+import com.sirkaue.hexagonalarchitecture.domain.exception.InvalidEmailException;
+
 public record Email(String value) {
+
+    public Email {
+        if (value == null || !value.matches("[^@]+@[^\\.]+\\..+")) {
+            throw new InvalidEmailException("Invalid email format");
+        }
+        value = value.toLowerCase();
+    }
 }
